74f912d97030fa2c71a430ff9d66f6b0c39c775a
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-05-15 17:51:50 +0300

    slides

--- FoodOrderingApp/FoodOrderingApp/Views/Onboarding/OnboardingCollectionViewCell.swift
+++ FoodOrderingApp/FoodOrderingApp/Views/Onboarding/OnboardingCollectionViewCell.swift
@@ -9,4 +9,15 @@ import UIKit
 
 class OnboardingCollectionViewCell: UICollectionViewCell {
     
+    static let identifier = String(describing: OnboardingCollectionViewCell.self)
+    
+    @IBOutlet weak var slideImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var slideDescriptionLabel: UILabel!
+    
+    func setup (_ slide: OnboardingSlide) {
+        slideImageView.image = slide.image
+        titleLabel.text = slide.title
+        slideDescriptionLabel.text = slide.description
+    }
 }
--- FoodOrderingApp/FoodOrderingApp/Views/Onboarding/OnboardingViewController.swift
+++ FoodOrderingApp/FoodOrderingApp/Views/Onboarding/OnboardingViewController.swift
@@ -11,11 +11,26 @@ class OnboardingViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
+                
     }
     
     @IBAction func nextButtonClicked(_ sender: UIButton) {
         
     }
+}
+
+extension OnboardingViewController : UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return slides.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OnboardingCollectionViewCell.identifier , for: indexPath) as! OnboardingCollectionViewCell
+        cell.setup(slides[indexPath.row])
+        return cell
+    }
+    
     
 }
