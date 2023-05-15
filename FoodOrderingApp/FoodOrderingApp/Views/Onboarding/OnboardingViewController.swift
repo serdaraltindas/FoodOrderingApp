b3eaf6f9810f00dd03753af6c976d4ad3ee1f229
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-05-15 20:27:55 +0300

    secondScreen

--- FoodOrderingApp/FoodOrderingApp/Views/Onboarding/OnboardingViewController.swift
+++ FoodOrderingApp/FoodOrderingApp/Views/Onboarding/OnboardingViewController.swift
@@ -2,26 +2,45 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
-
+    
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var nextButton: UIButton!
     
     var slides: [OnboardingSlide] = []
     
-    var currentPage = 0
+    var currentPage = 0 {
+        didSet {
+            pageControl.currentPage = currentPage
+            if currentPage == slides.count - 1 {
+                nextButton.setTitle("Get Started", for: .normal)
+            }else{
+                nextButton.setTitle("Next", for: .normal)
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         slides = [
-        OnboardingSlide(title: "Delicious Dishes", description: "Experience a variety of amazing dishes from different cultures around the world.", image:UIImage(imageLiteralResourceName: "slide1")),
-        OnboardingSlide(title: "World-Class Chefs", description: "Our dishes are prepared by only the best.", image: UIImage(imageLiteralResourceName: "slide2")),
-        OnboardingSlide(title: "Instant World-Wide Delivery", description: "Your orders will be delivered instantly irrespective of your location around the world.", image: UIImage(imageLiteralResourceName: "slide3"))
+            OnboardingSlide(title: "Delicious Dishes", description: "Experience a variety of amazing dishes from different cultures around the world.", image:UIImage(imageLiteralResourceName: "slide1")),
+            OnboardingSlide(title: "World-Class Chefs", description: "Our dishes are prepared by only the best.", image: UIImage(imageLiteralResourceName: "slide2")),
+            OnboardingSlide(title: "Instant World-Wide Delivery", description: "Your orders will be delivered instantly irrespective of your location around the world.", image: UIImage(imageLiteralResourceName: "slide3"))
         ]
     }
     
     @IBAction func nextButtonClicked(_ sender: UIButton) {
         //Button!
+        if currentPage == slides.count - 1 {
+            let controller = storyboard?.instantiateViewController(withIdentifier: "HomeNC") as! UINavigationController
+            controller.modalPresentationStyle = .fullScreen
+            controller.modalTransitionStyle = .flipHorizontal
+            present(controller, animated: true)
+        }else{
+            currentPage += 1
+            let indexPath = IndexPath(item: currentPage , section: 0)
+            collectionView.scrollToItem(at: indexPath , at: .centeredHorizontally , animated: true)
+        }
     }
 }
 
@@ -43,6 +62,5 @@ extension OnboardingViewController : UICollectionViewDelegate, UICollectionViewD
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         let width = scrollView.frame.width
         currentPage = Int(scrollView.contentOffset.x / width)
-        pageControl.currentPage = currentPage
     }
 }
