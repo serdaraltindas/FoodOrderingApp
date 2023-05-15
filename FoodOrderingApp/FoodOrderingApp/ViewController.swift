016f19293ed21735260834838a1cbcca6f783f80
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-05-15 14:56:25 +0300

    AppIcon

---
+++ FoodOrderingApp/FoodOrderingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodOrderingApp
+//
+//  Created by Serdar Altındaş on 15.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
