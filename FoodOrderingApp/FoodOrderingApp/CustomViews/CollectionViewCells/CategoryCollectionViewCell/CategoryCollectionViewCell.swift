9f864c1984c12b7e8e0e20f30ea563795ae92864
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-05-16 14:41:39 +0300

    kingfisher

---
+++ FoodOrderingApp/FoodOrderingApp/CustomViews/CollectionViewCells/CategoryCollectionViewCell/CategoryCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryCollectionViewCell.swift
+//  FoodOrderingApp
+//
+//  Created by Serdar Altındaş on 16.05.2023.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var categoryTitleLabel: UILabel!
+    @IBOutlet weak var categoryImageView: UIImageView!
+    
+    func setup(category: DishCategory) {
+        
+    }
+}
---
+++ FoodOrderingApp/FoodOrderingApp/Models/DishCategory.swift
@@ -0,0 +1,13 @@
+//
+//  DishCategory.swift
+//  FoodOrderingApp
+//
+//  Created by Serdar Altındaş on 16.05.2023.
+//
+
+import Foundation
+
+struct DishCategory {
+    let id,name,image:String
+    
+}
