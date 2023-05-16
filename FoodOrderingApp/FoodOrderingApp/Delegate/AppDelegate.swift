63732f60991dfd99d6a5f02a87bfd123840f2194
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-05-16 10:22:28 +0300

    For clear background

--- FoodOrderingApp/FoodOrderingApp/Delegate/AppDelegate.swift
+++ FoodOrderingApp/FoodOrderingApp/Delegate/AppDelegate.swift
@@ -12,6 +12,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //For Clear Background!
+        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
+        UINavigationBar.appearance().shadowImage = UIImage()
+        UINavigationBar.appearance().tintColor = .black
         return true
     }
 
--- FoodOrderingApp/FoodOrderingApp/Views/Onboarding/OnboardingViewController.swift
+++ FoodOrderingApp/FoodOrderingApp/Views/Onboarding/OnboardingViewController.swift
@@ -27,6 +27,7 @@ class OnboardingViewController: UIViewController {
             OnboardingSlide(title: "World-Class Chefs", description: "Our dishes are prepared by only the best.", image: UIImage(imageLiteralResourceName: "slide2")),
             OnboardingSlide(title: "Instant World-Wide Delivery", description: "Your orders will be delivered instantly irrespective of your location around the world.", image: UIImage(imageLiteralResourceName: "slide3"))
         ]
+        pageControl.numberOfPages = slides.count
     }
     
     @IBAction func nextButtonClicked(_ sender: UIButton) {
