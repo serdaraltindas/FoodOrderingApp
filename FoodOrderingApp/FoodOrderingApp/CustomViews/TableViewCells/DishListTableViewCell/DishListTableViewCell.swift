6e1f462ef46de4cee99a710d58e73c67d066e4c7
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-05-25 22:50:57 +0300

    Fixed

--- FoodOrderingApp/FoodOrderingApp/CustomViews/TableViewCells/DishListTableViewCell/DishListTableViewCell.swift
+++ FoodOrderingApp/FoodOrderingApp/CustomViews/TableViewCells/DishListTableViewCell/DishListTableViewCell.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class DishListTableViewCell: UITableViewCell {
-
-
+    @IBOutlet weak var dishImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
     
+    func setup(dish: Dish) {
+        dishImageView.kf.setImage(with: dish.image?.asUrl)
+        titleLabel.text = dish.name
+        descriptionLabel.text = dish.decription
+    }
 }
