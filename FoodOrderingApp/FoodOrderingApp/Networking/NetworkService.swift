631e50a3121a93dd00925403d13ae08bb906759f
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-05-28 15:01:34 +0300

    networkService

---
+++ FoodOrderingApp/FoodOrderingApp/Networking/NetworkService.swift
@@ -0,0 +1,22 @@
+
+import Foundation
+
+struct NetworkService {
+    private func createRequest(route: Route, method: Method, parameters: [String: Any]? = nil) -> URLRequest? {
+        let urlString = Route.baseUrl + route.description
+        guard let url = urlString.asUrl else {return nil}
+        var urlRequest = URLRequest(url: url)
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.httpMethod = method.rawValue
+        
+        if let params = parameters {
+            switch method {
+            case .get:
+                <#code#>
+            case .post, .delete, .patch:
+                let bodyData = try? JSONSerialization.data(withJSONObject: params, options: [])
+                urlRequest.httpBody = bodyData
+            }
+        }
+    }
+}
---
+++ FoodOrderingApp/FoodOrderingApp/Networking/Route.swift
@@ -0,0 +1,13 @@
+
+import Foundation
+
+enum Route {
+    static let baseUrl = "https://yummie.glitch.me"
+    case temp
+    var description: String {
+        switch self {
+        case .temp: return "/temp"
+            
+        }
+    }
+}
